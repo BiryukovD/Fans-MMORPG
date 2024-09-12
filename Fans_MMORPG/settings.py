52f8f5503772e7c2fd93fbf78ab5bf65f62dda34
Dmitry <dim.bir2017@yandex.ru>
2024-09-12 16:08:12 +0300

    created dump

--- Fans_MMORPG/settings.py
+++ Fans_MMORPG/settings.py
@@ -23,12 +23,12 @@ SITE_ID = 1
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+z))z2silxzdxf&*9h)9cmb@+h0w3wj@$0f=sdk8qzkp-m1ltn'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['45.147.177.84']
+# ALLOWED_HOSTS = ['45.147.177.84']
 
 AUTH_USER_MODEL = "app.MyUser"
 # Application definition
